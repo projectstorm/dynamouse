6d2730ba1368aaff3f759df333ebef41dcc9fc13
Dylan Vorster <dylanvorster.dv@gmail.com>
2024-02-25 23:28:40 -0700

    formatting

--- src/menu.ts
+++ src/menu.ts
@@ -74,7 +74,7 @@ export const buildMenu = async (options: BuildMenuOptions) => {
   startupMenu.append(
     new MenuItem({
       label: 'Enabled',
-      type:'checkbox',
+      type: 'checkbox',
       checked: autoLaunchEnabled,
       click: async () => {
         if (autoLaunchEnabled) {
